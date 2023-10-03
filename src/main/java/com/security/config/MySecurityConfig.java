dbba82036787046a5e5c839f6fa6eed7ebf6ed4f
Jay Thakur <92983729+thakurjay1@users.noreply.github.com>
2023-10-03 12:15:58 +0530

    configure method implemented

--- src/main/java/com/security/config/MySecurityConfig.java
+++ src/main/java/com/security/config/MySecurityConfig.java
@@ -1,11 +1,39 @@
 package com.security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SuppressWarnings("deprecation")
 public class MySecurityConfig extends WebSecurityConfigurerAdapter{
-   static {
-	   System.out.println("");
-   }
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+       http
+           .csrf().disable()
+           .cors().disable()
+           .authorizeRequests()
+           .anyRequest()
+           .authenticated()
+           .and()
+           .httpBasic();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		auth.inMemoryAuthentication().withUser("jay").password(this.passwordEncoder().encode("jay")).roles("ROLE_NORMAL");
+		auth.inMemoryAuthentication().withUser("rishabh").password(this.passwordEncoder().encode("rishabh")).roles("ROLE_ADMIN");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(10);
+	}
+	
 }
